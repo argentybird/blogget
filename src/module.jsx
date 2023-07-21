2bd2b72ee053d677ecbcc91b40bbda1e68be6d91
Marina Emelianova <argenty@gmail.com>
2023-07-21 18:48:09 +0300

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import msk from "./img/moscow.jpg"
+const Module = () => (
+    <>
+        <h1>
+            Hello World
+        </h1>
+        <img src={msk} width={500}/>
+    </>
+);
+
+export default Module;
